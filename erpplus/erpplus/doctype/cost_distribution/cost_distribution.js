13a7f6f59fdf0a454dc68953e5b59f2ce7751482
dachir <dodziamouzou@gmail.com>
2023-11-18 14:26:27 +0000

    181120231526

--- erpplus/erpplus/doctype/cost_distribution/cost_distribution.js
+++ erpplus/erpplus/doctype/cost_distribution/cost_distribution.js
@@ -2,14 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Cost Distribution', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function (frm) {
+        // Make the first row of the child table readonly
+        frm.fields_dict['details'].grid.get_field('type').read_only = function(doc, cdt, cdn) {
+            // Check if the current row is the first row
+            return locals[cdt][cdn].idx === 1;
+        };
+    },
 	distribution: function(frm) {
 		if (frm.doc.distribution){
 			frm.call('fill_details');
 		}
-	}
+	},
 });
 
 frappe.ui.form.on('Cost Distribution Details', {
--- erpplus/erpplus/doctype/cost_distribution/cost_distribution.py
+++ erpplus/erpplus/doctype/cost_distribution/cost_distribution.py
@@ -2,40 +2,158 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
+from frappe.utils import getdate
+from frappe.utils import flt
 from frappe.model.document import Document
 
 class CostDistribution(Document):
+	def on_submit(self):
+		self.make_accrual_jv_entry()
+
+	def validate(self):
+		for d in self.get("details"):
+			if d.type == "Parent":
+				if (d.debit != d.debit_amount) or (d.credit != d.credit_amount):
+					frappe.throw(_("Ligne {0}: Parent Lines should not be changed").format(d.idx))
+
+	def on_cancel(self):
+		nb = frappe.db.count("Journal Entry",  {"cheque_no" : self.name})
+		if nb > 0:
+			jv = frappe.get_doc("Journal Entry", {"cheque_no" : self.name})
+			jv.cancel()
 	
 	@frappe.whitelist()
 	def fill_details(self):
+		self.details.clear()
 		gl_doc = frappe.get_doc("GL Entry", self.document)
 		names = frappe.db.get_list("GL Entry",["name"], {"voucher_no":gl_doc.voucher_no})
-        doctype = gl_doc.get("doctype")
+		doctype = gl_doc.get("doctype")
 		for name in names:
 			gl_doc = frappe.get_doc("GL Entry", name)
-			parent_dimension = gl_doc.get(self.dimension.lower())
-			if parent_dimension:
-				tpl_doc = frappe.get_doc("Cost Distribution Template", {"name":self.distribution, "dimension": parent_dimension})
-				if tpl_doc :
-					self.append('details',{
-							"document_type": self.dimension,
-							"dimension": parent_dimension,
-							"account": gl_doc.account,
-							"debit_amount": self.credit if self.credit > 0 else 0,
-							"credit_amount": self.debit if self.debit > 0 else 0,
-							"type": "Parent",
-						}
-					)
-					
-					for l in tpl_doc.details:
+			expense_account = frappe.get_value("Account", {"name": gl_doc.account, "root_type": "Expense", "account_type": ["!=", "Round Off"]}, "name")
+			if expense_account:
+				parent_dimension = gl_doc.get(self.dimension.lower())
+				if parent_dimension:
+					tpl_doc = frappe.get_doc("Cost Distribution Template", {"name":self.distribution, "parent1": parent_dimension})
+					if tpl_doc :	
 						self.append('details',{
 								"document_type": self.dimension,
-								"dimension": l.dimension,
-								"account": gl_doc.account,
-								"debit_amount": self.debit if self.debit > 0 else 0 * l.rate / 100,
-								"credit_amount": self.credit if self.credit > 0 else 0 * l.rate / 100,
-								"type": "Child",
+								"dimension": parent_dimension,
+								"account": expense_account,
+								"debit_amount": gl_doc.credit if gl_doc.credit > 0 else 0,
+								"credit_amount": gl_doc.debit if gl_doc.debit > 0 else 0,
+								"debit": gl_doc.credit if gl_doc.credit > 0 else 0,
+								"credit": gl_doc.debit if gl_doc.debit > 0 else 0,
+								"type": "Parent",
+								"gl_entry": gl_doc.name,
 							}
 						)
+						
+						for l in tpl_doc.details:
+							self.append('details',{
+									"document_type": self.dimension,
+									"dimension": l.dimension,
+									"account": expense_account,
+									"debit_amount": (gl_doc.debit if gl_doc.debit > 0 else 0) * l.rate / 100,
+									"credit_amount": (gl_doc.credit if gl_doc.credit > 0 else 0) * l.rate / 100,
+									"debit": (gl_doc.debit if gl_doc.debit > 0 else 0) * l.rate / 100,
+									"credit": (gl_doc.credit if gl_doc.credit > 0 else 0) * l.rate / 100,
+									"type": "Child",
+									"gl_entry": gl_doc.name,
+								}
+							)
+
+
+	def create_row(self,r):
+		row = {
+			"account": r.account,
+			#"exchange_rate": ex_rate,
+			#"reference_type": self.doctype,
+			#"reference_name": self.name,
+			"debit_in_account_currency": r.debit_amount,
+			"credit_in_account_currency": r.credit_amount,
+			#"reference_currency": self.devise,
+			#"reference_rate": cours,
+			#"reference_amount": amount,
+		}
+		
+		gle = frappe.get_doc("GL Entry", r.gl_entry)
+		doctype = gle.get("doctype")
+		fieldnames = frappe.get_meta(doctype).get_valid_columns()
+		idx = fieldnames.index("is_cancelled")
+		nb = len(fieldnames)
+		dimensions = fieldnames[idx + 1:nb]
+		for d in dimensions:
+			if gle.get(d) :
+				if d == self.dimension.lower() :
+					row.update(
+						{
+							d: r.dimension,
+						}
+					)
+				else :
+					row.update(
+						{
+							d: gle.get(d),
+						}
+					)
+		
+		return row
+
+
+	def make_accrual_jv_entry(self):
+		precision = frappe.get_precision("Journal Entry Account", "debit_in_account_currency")
+		journal_entry = frappe.new_doc("Journal Entry")
+		journal_entry.voucher_type = "Journal Entry"
+		gle = frappe.get_doc("GL Entry", self.document)
+		journal_entry.user_remark = _("Cost Distribution on  {0} for dimension {1}").format(gle.voucher_no, self.dimension) 
+		journal_entry.company = self.company 
+		journal_entry.posting_date = gle.posting_date
+		journal_entry.cheque_no = self.name
+		journal_entry.cheque_date = self.date
+		accounts = []
+		payable_amount = 0
+		row = {}
+
+		for d in self.details:
+			amount = flt(d.debit_amount - d.credit_amount, precision)
+			payable_amount += amount
+
+			accounting_entry = self.create_row(d)
+			accounts.append(accounting_entry)
+
+		if flt(payable_amount, precision) != 0 :
+			round_off_account = self.get_account("Company", self.company,"round_off_account")
+			row = {
+				"account": accounting_entry,
+				"debit_in_account_currency": (flt(payable_amount, precision) if flt(payable_amount, precision) < 0 else 0) * -1,
+				"credit_in_account_currency": flt(payable_amount, precision) if flt(payable_amount, precision) >= 0 else 0,
+			}
+		
+			doctype = gle.get("doctype")
+			fieldnames = frappe.get_meta(doctype).get_valid_columns()
+			idx = fieldnames.index("is_cancelled")
+			nb = len(fieldnames)
+			dimensions = fieldnames[idx + 1:nb]
+			for d in dimensions:
+				if gle.get(d) :
+					if d == self.dimension :
+						row.update(
+							{
+								d: r.dimension,
+							}
+						)
+					else :
+						row.update(
+							{
+								d: gle.get(d),
+							}
+						)
+				accounts.append(row)
+			
+		journal_entry.title = _("Cost Distribution on  {0} for dimension {1}").format(gle.voucher_no, self.dimension)
+		journal_entry.set("accounts", accounts)
+		journal_entry.submit()
 				
 			
