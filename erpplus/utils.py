48bbe41c43ba06f463aa3336e3598d394f22f208
dachir <dodziamouzou@gmail.com>
2023-12-11 18:01:30 +0000

    111220231901

--- erpplus/utils.py
+++ erpplus/utils.py
@@ -109,7 +109,7 @@ def get_provisions_from_gl(params=None):
 	condition_str = " AND ".join(conditions)
 
 	query = f"""
-		SELECT name, account, credit AS amount, remaining_amount
+		SELECT name,voucher_no, account, credit AS amount, remaining_amount
 		FROM `tabGL Entry`
 		WHERE {condition_str}
 	"""
