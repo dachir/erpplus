9a13145d10c53b82b8a0767ea561f4418e116e44
dachir <dodziamouzou@gmail.com>
2024-02-01 09:31:49 +0000

    010220241031

--- erpplus/overrides/stock_entry.py
+++ erpplus/overrides/stock_entry.py
@@ -113,14 +113,14 @@ class CustomStockEntry(StockEntry):
 					row = {
 						"account": warehouses[d.s_warehouse].account,
 						"debit_in_account_currency": 0,
-						"credit_in_account_currency": d.amount,
+						"credit_in_account_currency": d.basic_amount,
 						"branch": s_branch,
 					}
 					accounts.append(row)
 					stock_transfert_account = frappe.db.get_value("Branch", s_branch, "stock_transfert_account")
 					row = {
 						"account": stock_transfert_account,
-						"debit_in_account_currency": d.amount,
+						"debit_in_account_currency": d.basic_amount,
 						"credit_in_account_currency": 0,
 						"branch": s_branch,
 					}
@@ -128,7 +128,7 @@ class CustomStockEntry(StockEntry):
 			else:
 				row = {
 					"account": warehouses[d.t_warehouse].account,
-					"debit_in_account_currency": d.amount,
+					"debit_in_account_currency": d.basic_amount,
 					"credit_in_account_currency": 0,
 					"branch": t_branch,
 				}
@@ -137,7 +137,65 @@ class CustomStockEntry(StockEntry):
 				row = {
 					"account": stock_transfert_account,
 					"debit_in_account_currency": 0,
-					"credit_in_account_currency": d.amount,
+					"credit_in_account_currency": d.basic_amount,
+					"branch": t_branch,
+				}
+				accounts.append(row)
+	
+		if row :	
+			journal_entry.title = _("Stock Transfert {0} ").format(self.name) 
+			journal_entry.set("accounts", accounts)
+			journal_entry.flags.ignore_permissions = 1
+			journal_entry.submit()
+
+
+
+
+	def correction_jv_entry(self):
+		journal_entry = frappe.new_doc("Journal Entry")
+		journal_entry.voucher_type = "Journal Entry"
+		journal_entry.user_remark = _("Stock Transfert {0} ").format(self.name) 
+		journal_entry.company = self.company 
+		journal_entry.posting_date = self.posting_date
+		journal_entry.cheque_no = self.name
+		journal_entry.cheque_date = self.posting_date
+		accounts = []
+		row = {}
+		warehouses = get_warehouse_account_map(self.company)
+
+		for d in self.items:
+			s_branch = frappe.db.get_value("Warehouse", d.s_warehouse, "branch")
+			t_branch = frappe.db.get_value("Warehouse", d.t_warehouse, "branch")
+			if s_branch == t_branch :
+				if self.add_to_transit:
+					row = {
+						"account": warehouses[d.s_warehouse].account,
+						"debit_in_account_currency": 0,
+						"credit_in_account_currency": -d.additional_cost,
+						"branch": s_branch,
+					}
+					accounts.append(row)
+					stock_transfert_account = frappe.db.get_value("Branch", s_branch, "stock_transfert_account")
+					row = {
+						"account": stock_transfert_account,
+						"debit_in_account_currency": -d.additional_cost,
+						"credit_in_account_currency": 0,
+						"branch": s_branch,
+					}
+					accounts.append(row)
+			else:
+				row = {
+					"account": warehouses[d.t_warehouse].account,
+					"debit_in_account_currency": -d.additional_cost,
+					"credit_in_account_currency": 0,
+					"branch": t_branch,
+				}
+				accounts.append(row)
+				stock_transfert_account = frappe.db.get_value("Branch", t_branch, "stock_transfert_account")
+				row = {
+					"account": stock_transfert_account,
+					"debit_in_account_currency": 0,
+					"credit_in_account_currency": -d.additional_cost,
 					"branch": t_branch,
 				}
 				accounts.append(row)
