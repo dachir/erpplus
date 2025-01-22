22501368979ab426a4d129545f6cc6f410bd1d92
dachir <dodziamouzou@gmail.com>
2025-01-22 17:38:51 +0000

    220120251701

---
+++ erpplus/erpplus/doctype/stock_simulation/stock_simulation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Kossivi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class StockSimulation(Document):
+	pass
