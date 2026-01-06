4c5e594ef13477be8ea07d8eef7e60a205c80456
dachir <dodziamouzou@gmail.com>
2026-01-06 08:20:28 +0000

    060120260919

--- erpplus/overrides/purchase_invoice.py
+++ erpplus/overrides/purchase_invoice.py
@@ -12,11 +12,11 @@ class CustomPurchaseInvoice(PurchaseInvoice):
                 if remaining_amount < i.amount:
                     frappe.throw("The amount enter is more than the remaining amount")
 
-                frappe.db.update("GL Entry", i.gl_entry, "remaining_amount",remaining_amount - i.amount)
+                frappe.db.update("GL Entry", i.gl_entry, "remaining_amount",(remaining_amount or 0.0) - (i.amount or 0.0))
 
     def on_cancel(self):
         super(CustomPurchaseInvoice, self).on_cancel()
         for i in self.items:
             if i.gl_entry :
                 remaining_amount = frappe.db.get_value("GL Entry", i.gl_entry, "remaining_amount")
-                frappe.db.update("GL Entry", i.gl_entry, "remaining_amount",remaining_amount + i.amount)
+                frappe.db.update("GL Entry", i.gl_entry, "remaining_amount",(remaining_amount or 0.0) + (i.amount or 0.0))
